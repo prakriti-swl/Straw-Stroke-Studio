e890e8b6393913e2ae21654fad477914a31b5ff6
Prakriti <suwalprakriti2004@gmail.com>
2025-12-14 16:53:57 +0545

    [Added} -gallery -gallery detail [Modified] - intro -contact -footer -index

--- strawandstroke_app/models.py
+++ strawandstroke_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Gallery(TimestampedModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='gallery/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='galleries')
+
+    def __str__(self):
+        return self.title
--- strawandstroke_app/views.py
+++ strawandstroke_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Gallery
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "index.html"
+
+class GalleryView(TemplateView):
+    model = Gallery
+    template_name = "gallery.html"
+    context_object_name = "galleries"
+
+    def get_queryset(self):
+        return Gallery.objects.filter(
+            published_at__isnull=False
+        ).order_by('-published_at')
+    
+
+class GalleryDetailView(TemplateView):
+    model = Gallery
+    template_name = "gallery_detail.html"
+    context_object_name = "gallery"
+
+    def get_queryset(self):
+        return Gallery.objects.filter(
+            published_at__isnull=False
+        )
